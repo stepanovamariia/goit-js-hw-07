ce322728e8678dd95c2164dc5f11ed0e6c50ecc2
stepanovamariia <mariia.alexsa@gmail.com>
2023-10-26 11:02:47 +0300

    123

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryList = document.querySelector('.gallery');
+const galleryMarkup = createGallery(galleryItems);
+
+galleryList.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGallery(items) {
+	return items
+		.map(({ preview, original, description }) => {
+			return `
+            <li class="gallery__item">
+             <a class="gallery__link" href="${original}"  >
+              <img class="gallery__image" src="${preview}" alt="${description}" title="${description}" />
+             </a>
+            </li>
+            `;
+		})
+		.join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+	captions: true,
+	captionSelector: 'img',
+	captionType: 'attr',
+	captionsData: alt,
+	captionDelay: 250,
+	captionPosition: 'bottom',
+});
+
 console.log(galleryItems);
